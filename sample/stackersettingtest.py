6a2083347a1895e9e77b15f5e1b3dd1004c16884
Belre-Yucho <neopossam@gmail.com>
2018-09-07 20:02:23 +0900

    秘匿情報削除

--- sample/stackersettingtest.py
+++
@@ -1,5 +0,0 @@
-import json
-
-jsonmsg = json.dumps({'Stacker1': ['CAT 1', 'CAT 2'], 'Stacker2' : None, 'Stacker3' : None, 'Stacker4' : ['CAT 8', 'CAT 11'], 'Stacker5' : ['CAT 6'], 'Stacker6' : ['Other'], 'Stacker7' : ['Unknown'], 'Stacker8' : ['mixed']}, ensure_ascii=False)
-print(jsonmsg)    
-
